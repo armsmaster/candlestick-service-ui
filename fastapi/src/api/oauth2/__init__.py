7781acc0ab0fa609ef52dc79d67529bb480713eb
Edward <edward.gordin@gmail.com>
2025-04-14 17:25:45 +0300

    move oauth yandex handles to separate module

--- fastapi/src/api/oauth2/__init__.py
+++ fastapi/src/api/oauth2/__init__.py
@@ -5,6 +5,8 @@ __all__ = [
 from fastapi import APIRouter
 
 from src.api.oauth2.google import api_router as api_router_oauth_google
+from src.api.oauth2.yandex import api_router as api_router_oauth_yandex
 
 router_oauth = APIRouter(prefix="/oauth2", tags=["oauth"])
 router_oauth.include_router(api_router_oauth_google)
+router_oauth.include_router(api_router_oauth_yandex)
